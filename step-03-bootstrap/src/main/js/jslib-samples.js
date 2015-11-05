8aad19b0a78b40297c7090fd2d600ba3fd40d506
tfennelly <tom.fennelly@gmail.com>
2015-11-05 00:15:55 +0000

    Add rest of step-03-bootstrap - using bootstrap in the scripts

--- step-03-bootstrap/src/main/js/jslib-samples.js
+++ step-03-bootstrap/src/main/js/jslib-samples.js
@@ -1,6 +1,9 @@
-var $ = require('jquery-detached').getJQuery();
+var $ = require('bootstrap-detached').getBootstrap();
 
 $(document).ready(function () {    
     $('#side-panel').remove();
     $('#main-panel').css('margin-left', '0px');
+    
+    var moment = require('moment');
+    $('#main-panel .time').text(moment().format("MMM Do YY"));
 });
