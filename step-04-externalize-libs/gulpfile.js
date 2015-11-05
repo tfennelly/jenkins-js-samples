d6885105441305b1921004affe0edaed2b49699c
tfennelly <tom.fennelly@gmail.com>
2015-11-05 00:46:31 +0000

    Add rest of step-04-externalize-libs - now the jslibs-samples.js is slim

--- step-04-externalize-libs/gulpfile.js
+++ step-04-externalize-libs/gulpfile.js
@@ -5,4 +5,6 @@ var builder = require('jenkins-js-builder');
 // See https://github.com/tfennelly/jenkins-js-builder
 //
 builder.bundle('src/main/js/jslib-samples.js')
+       .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
+       .withExternalModuleMapping('moment', 'momentjs:momentjs2')
        .inDir('src/main/webapp/jsbundles');
