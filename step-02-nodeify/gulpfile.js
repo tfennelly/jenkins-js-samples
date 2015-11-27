6a18238bc56104553027939481f9ee29f477a056
tfennelly <tom.fennelly@gmail.com>
2015-11-27 12:19:26 +0000

    More docs

--- step-02-nodeify/gulpfile.js
+++ step-02-nodeify/gulpfile.js
@@ -2,7 +2,7 @@ var builder = require('jenkins-js-builder');
 
 //
 // Bundle the modules.
-// See https://github.com/tfennelly/jenkins-js-builder
+// See https://github.com/jenkinsci/js-builder
 //
 builder.bundle('src/main/js/jslib-samples.js')
        .inDir('src/main/webapp/jsbundles');
