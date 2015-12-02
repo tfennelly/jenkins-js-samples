c5e2d70d0fbc2fda0df0f91dd07342b209bf6fd0
tfennelly <tom.fennelly@gmail.com>
2015-12-02 14:42:44 +0000

    Modified to do a popover

--- step-07-jsdom-tests/src/main/js/jslib-samples.js
+++ step-07-jsdom-tests/src/main/js/jslib-samples.js
@@ -11,7 +11,21 @@ $(document).ready(function () {
     // Apply the 'form' template, appending the resulting content to
     // the 'form-container' element. We could also define a Handlers helper
     // to handle the formatted time.
-    $('#form-container').append(formTemplate({
-        time: moment().format("MMM Do YY")
-    }));
+    $('#submit-details').click(function() {
+        var popoverContent = formTemplate({
+            time: moment().format("MMM Do YY")
+        });
+        $('#submit-details').popover({
+            title: "Please submit your details",
+            content: popoverContent,
+            html: true,
+            trigger: 'manual'
+        });
+        $('#submit-details').on('shown.bs.popover', function () {
+            $('#submit-details-go').click(function() {
+                $('#submit-details').popover('destroy');
+            });
+        });        
+        $('#submit-details').popover('show');
+    });
 });
